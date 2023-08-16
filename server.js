8c4fd36c697aa3f2c0f997e7e80b0f905a8c6bd8
Tuckers files <christole@chrissmacstudio.lan>
2023-08-16 12:55:34 -0500

    1

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path'); 
 const fs = require('fs');
-const dbData = require('.db/db.json');
+const dbData = require('./develop/db/db.json');
 
 const PORT = 3002;
 
